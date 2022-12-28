1a7d3c1f73fb41c44b25563430e07a76fc67a3d1
cutejoon <s22073@gsm.hs.kr>
2022-12-28 20:09:19 +0900

    retry domain

--- src/components/mainheader.js
+++ src/components/mainheader.js
@@ -1,59 +1,56 @@
-import main from "./css/main.module.css"
+import main from "./css/main.module.css";
 import { Link } from "react-router-dom";
 import logo from "./img/logo.png";
 
-export default function Mainheader(){
-    return(
-        <div>
-            <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"
-        />
-        <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"
-        />
-        <div className={main.header}>
-            <Link to={`/main`}>
-            <img src={logo} className={main.logo} alt="Mo.zip" />
-            </Link>
-            <Link to={`/category1`} >
-            <button className={main.category1}>
-            운동
-            </button>
-            </Link>
-            <Link to={`/category2`} >
-            <button className={main.category2}>
-            프로젝트
-            </button>
-            </Link>
-            <Link to={`/category3`} >
-            <button className={main.category3}>
-            공동구매
-            </button>
-            </Link>
-            <Link to={`/mypage`}>
-            <button className={main.myinfo}>
-                <span class="material-symbols-outlined" id={main.myinfoicon}>
-                person
-                </span>
-                한준
-            </button>
-            </Link>
-            <Link to={`/login`}>
-            <button className={main.logout}>
-                <span className="material-symbols-outlined" id={main.logouticon}>
-                logout
-                </span>
-                Log out
-            </button>
-            </Link>
-            <hr className={main.navbar}></hr>
-        </div>
-        <div className={main.body}>
-        
-        
+export default function Mainheader({ users }) {
+  return (
+    <div>
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"
+      />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"
+      />
+      <div className={main.header}>
+        <Link to={`/main`}>
+          <img src={logo} className={main.logo} alt="Mo.zip" />
+        </Link>
+        <Link to={`/category1`}>
+          <button className={main.category1}>운동</button>
+        </Link>
+        <Link to={`/category2`}>
+          <button className={main.category2}>프로젝트</button>
+        </Link>
+        <Link to={`/category3`}>
+          <button className={main.category3}>공동구매</button>
+        </Link>
+        <Link to={`/mypage`}>
+          <button className={main.myinfo}>
+            <span className="material-symbols-outlined" id={main.myinfoicon}>
+              person
+            </span>
+            {users.map((user) => {
+              return (
+                <div key={user.id} className={main.myinfo}>
+                  {user.name}
+                </div>
+              );
+            })}
+          </button>
+        </Link>
+        <Link to={`/login`}>
+          <button className={main.logout}>
+            <span className="material-symbols-outlined" id={main.logouticon}>
+              logout
+            </span>
+            Log out
+          </button>
+        </Link>
+        <hr className={main.navbar}></hr>
       </div>
-        </div>
-    );
+      <div className={main.body}></div>
+    </div>
+  );
 }
--- src/components/mainpage.js
+++ src/components/mainpage.js
@@ -1,32 +1,32 @@
 import { Link } from "react-router-dom";
 import main from "./css/main.module.css";
 import Mainheader from "./mainheader";
-import { useEffect, useState } from "react";
 import axios from "axios";
+import { useEffect, useState } from "react";
 import meet from "./img/meet.png";
 
-const SERVER_URL = "http://ec2-52-79-236-28.ap-northeast-2.compute.amazonaws.com/";
+const SERVER_URL = "http://mo-zip.online/user-controller";
 
 function Mainpage() {
-  const onSubmitHandler = async (e) => {
-    e.preventDefault();
-    await axios.get(SERVER_URL, { }).then((res)=>{
-      console.log(res.data);
-    }).catch(alert("사용자 정보가 없습니다."));
-    
-  };
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    axios.get(SERVER_URL).then((response) => {
+      setUsers(response.data);
+    });
+  }, []);
+
   return (
     <div className={main.main}>
-      <Mainheader />
+      <Mainheader users={users}/>
       <h1 className={main.find}>
-          내가 찾던 파트너와
-          <br />
-          함께하세요!
-        </h1>
+        내가 찾던 파트너와
+        <br />
+        함께하세요!
+      </h1>
       <Link to={`/write`}>
-          <button className={main.btn}>Mo.zip 하기</button>
-        </Link>
-      <img src={meet} className={main.meet} alt="Mo.zip"/>
+        <button className={main.btn}>Mo.zip 하기</button>
+      </Link>
+      <img src={meet} className={main.meet} alt="Mo.zip" />
     </div>
   );
 }
