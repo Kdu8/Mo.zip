c7fd0791740ac6556ea8bf7d830e29e10ab47b4f
cutejoon <s22073@gsm.hs.kr>
2022-12-29 14:05:17 +0900

    check userdat

--- src/components/mainheader.js
+++ src/components/mainheader.js
@@ -6,9 +6,37 @@ export default function Mainheader({ user }) {
   console.log(user);
   return (
     <div>
-      {user.map(user => {
-        return <div key={user.id}>{user.name}</div>;
-      })}
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"
+      />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"
+      />
+      <div className={main.header}>
+        <Link to={`/main`}>
+          <img src={logo} className={main.logo} alt="Mo.zip" />
+        </Link>
+        <Link to={`/mypage`}>
+          <button className={main.myinfo}>
+            <span className="material-symbols-outlined" id={main.myinfoicon}>
+              person
+            </span>
+            한준
+          </button>
+        </Link>
+        <Link to={`/login`}>
+          <button className={main.logout}>
+            <span className="material-symbols-outlined" id={main.logouticon}>
+              logout
+            </span>
+            Log out
+          </button>
+        </Link>
+        <hr className={main.navbar}></hr>
+      </div>
+      <div className={main.body}></div>
     </div>
   );
 }
