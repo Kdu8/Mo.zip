fefe1d1ce51b238d52697ff910c71cd4ca11712d
cutejoon <s22073@gsm.hs.kr>
2022-12-30 21:59:43 +0900

    grid top left

--- src/components/category1page.js
+++ src/components/category1page.js
@@ -61,6 +61,8 @@ function Category1page() {
           rowGap: "1.8rem",
           width: "800px",
           height: "250px",
+          top: "-50%",
+          left: "100%",
         }}
       >
         {boardlist.map((ele) => {
