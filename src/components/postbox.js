70888c01ba1faad7f66a2fd33b3dfd38c149b217
cutejoon <s22073@gsm.hs.kr>
2022-12-30 15:55:22 +0900

    Link replace

--- src/components/postbox.js
+++ src/components/postbox.js
@@ -2,40 +2,42 @@ import postbox from "./css/postbox.module.css";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
-export default function Postbox({board}) {
-  if(board !== undefined && board.exDate !== undefined){
+export default function Postbox({ board }) {
+  if (board !== undefined && board.exDate !== undefined) {
     let moziping = "Mo.zip";
-    if(board.finished){
+    if (board.finished) {
       moziping = "마감완료";
     }
-  const newexDate = new Date(board.exDate);
-  const year = newexDate.getFullYear();
-  const month = newexDate.getMonth();
-  const day = newexDate.getDate();
-  return (
-    <div className={board.id} id={board.body}>
-      <div className={postbox.postbox}>
-        <Link to={`/postpage`} className={postbox.awidth}>
+    const newexDate = new Date(board.exDate);
+    const year = newexDate.getFullYear();
+    const month = newexDate.getMonth();
+    const day = newexDate.getDate();
+    return (
+      <div className={board.id} id={board.body}>
+        <div className={postbox.postbox}>
           <div className={postbox.postbox1}>
-            <p className={postbox.headline}>{board.title}</p>
-            <p className={postbox.inline}>
-              {board.content}
-            </p>
+            <Link to={`/postpage`} className={postbox.awidth}>
+              <p className={postbox.headline}>{board.title}</p>
+            </Link>
+            <p className={postbox.inline}>{board.content}</p>
             <hr className={postbox.postdiv}></hr>
             <p className={postbox.deadline}>마감일</p>
-            <p className={postbox.deadlinedate}>{`${year}년 ${month + 1}월 ${day}일`}</p>
+            <p className={postbox.deadlinedate}>{`${year}년 ${
+              month + 1
+            }월 ${day}일`}</p>
             <p className={postbox.postuser}>작성자</p>
             <p className={postbox.postusername}>{board.writerName}</p>
-            <p className={postbox.people}>{`${board.applicantCount}/${board.maxApp}`}</p>
+            <p
+              className={postbox.people}
+            >{`${board.applicantCount}/${board.maxApp}`}</p>
             <button className={postbox.state}>{moziping}</button>
           </div>
-        </Link>
-    </div>
-    </div>
-  );
-}
+        </div>
+      </div>
+    );
+  }
 }
 
 Postbox.prototype = {
   exDate: PropTypes.string.isRequired,
-}
+};
