a591b3a086b1146e6842f087af25d56a86328f6f
cutejoon <s22073@gsm.hs.kr>
2023-01-02 08:19:56 +0900

    exDate

--- src/components/board.js
+++ src/components/board.js
@@ -1,12 +1,11 @@
 import Mainheader from "./mainheader";
 import postpage from "./css/postpage.module.css";
-import Userlist from "../components/userlist"
+import Userlist from "../components/userlist";
 import arrow from "./img/arrow.png";
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
-
 
 const SERVER_URL = "https://api.mo-zip.online/users/me";
 
@@ -48,48 +47,51 @@ export default function Postpage() {
       });
   };
   if (boardget?.board?.exDate) {
+    const newexDate = new Date(boardget.board.exDate);
+    const year = newexDate.getFullYear();
+    const month = newexDate.getMonth();
+    const day = newexDate.getDate();
+    return (
+      <div className={postpage.body}>
+        <Mainheader user={user} />
 
-  const newexDate = new Date(boardget.board.exDate);
-  const year = newexDate.getFullYear();
-  const month = newexDate.getMonth();
-  const day = newexDate.getDate();
-  return (
-    <div className={postpage.body}>
-      <Mainheader user={user}/>
-
-      <div className={postpage.postcontainer}>
-        <div className={postpage.titlebox}>
-        <p className={postpage.title}>{boardget.board.title}</p>
+        <div className={postpage.postcontainer}>
+          <div className={postpage.titlebox}>
+            <p className={postpage.title}>{boardget.board.title}</p>
+          </div>
+          <div>
+            <div className={postpage.writerbox}>
+              <p className={postpage.name}>작성자: {boardget.writerName}</p>
+            </div>
+            <div className={postpage.exDatebox}>
+              <p className={postpage.exDate}>
+                마감일 : {`${year}년 ${month + 1}월 ${day}일`}
+              </p>
+            </div>
+            <div className={postpage.maxAppbox}>
+              <p className={postpage.maxApp}>
+                모집인원 : {boardget.board.maxApp}
+              </p>
+            </div>
+          </div>
+          <div className={postpage.contentbox}>
+            <p className={postpage.content}>{boardget.board.content}</p>
+          </div>
+          <div className={postpage.needbox}>
+            <p className={postpage.need}>{boardget.board.requirement}</p>
+          </div>
+          <Userlist userlist={boardget.users} />
         </div>
-        <div className={postpage.writerbox}>
-        <p className={postpage.name}>작성자: {boardget.writerName}</p>
-        </div>
-        <div className={postpage.exDatebox}>
-        <p className={postpage.exDate}>
-          마감일 : {`${year}년 ${month + 1}월 ${day}일`}
-        </p>
-        </div>
-        <div className={postpage.maxAppbox}>
-        <p className={postpage.maxApp}>모집인원 : {boardget.board.maxApp}</p>
-        </div>
-        <div className={postpage.contentbox}>
-        <p className={postpage.content}>{boardget.board.content}</p>
-        </div>
-        <div className={postpage.needbox}>
-        <p className={postpage.need}>{boardget.board.requirement}</p>
-        </div>
-        <Userlist userlist={boardget.users}/>
-      </div>
-      <div className={postpage.arrowmain}>
+        <div className={postpage.arrowmain}>
           <button onClick={ApplySubmit} className={postpage.apply}>
             신청/신청취소
           </button>
-        <Link to={`/main`}>
-          <img src={arrow} alt="나가기" className={postpage.arrow} />
-          <p className={postpage.gomain}>나가기</p>
-        </Link>
+          <Link to={`/main`}>
+            <img src={arrow} alt="나가기" className={postpage.arrow} />
+            <p className={postpage.gomain}>나가기</p>
+          </Link>
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 }
