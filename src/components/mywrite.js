68dab0ec00208e17bab209387954115203f0a732
cutejoon <s22073@gsm.hs.kr>
2023-01-01 22:04:13 +0900

    button css

--- src/components/mywrite.js
+++ src/components/mywrite.js
@@ -5,8 +5,8 @@ const Mywrite = ({ write }) => {
   return (
     <div className={mypage.listbox}>
       <div className={mypage.listtitle}>{write.title}</div>
-      <button className={mypage.editbtn}>수정</button>
-      <button className={mypage.removebtn}>삭제</button>
+      <button className={mypage.editbtn} id={mypage.button}>수정</button>
+      <button className={mypage.removebtn} id={mypage.button}>삭제</button>
     </div>
   );
 };
