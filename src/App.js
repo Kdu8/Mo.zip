7ba2f7643bd94f0f2b630e26cc3e5b0b847224fb
cutejoon <s22073@gsm.hs.kr>
2023-01-01 21:02:33 +0900

    useparams

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Category1 from "./routes/category1";
 import Category2 from "./routes/category2";
 import Category3 from "./routes/category3";
 import Check from "./routes/check";
-import Userlist from "./components/userlist"
+import Userlist from "./components/userlist";
 import { Routes } from "react-router";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Postpageview from "./routes/postpageview";
--- src/components/board.js
+++ src/components/board.js
@@ -8,14 +8,16 @@ import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
 
 const SERVER_URL = "https://api.mo-zip.online/boards/";
-export default function Postpage({boardid}) {
+export default function Postpage() {
+  const {id} = useParams();
   const [board, setBoard] = useState({});
   const [isLoaded, setIsLoaded] = useState(false);
   const navigate = useNavigate();
   const [show, setShow] = useState(false);
 
   useEffect(() => {
-    axios.get("https://api.mo-zip.online/boards/"+boardid).then(res => {
+    console.log(id);
+    axios.get("https://api.mo-zip.online/boards/"+id).then(res => {
       console.log(res.data)
     })
   }, []);
